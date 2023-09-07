be714ffa7f0485a2b04c3f364b88f93b76b63d74
Dominik Ridjic <dominik.ridjic@sparrowlabs.at>
2023-09-07 23:07:29 +0200

    Update Github workflow and Package.swift

--- Package.swift
+++ Package.swift
@@ -1,7 +1,12 @@
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
     name: "CallAnalyticsSdk",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v10_14)
+        ],
     products: [
         .library(
             name: "CallAnalyticsSdk",
@@ -11,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CallAnalyticsSdk",
-            path: "./CallAnalyticsSdk.xcframework"
+            path: "CallAnalyticsSdk.xcframework"
         ),
     ]
 )
