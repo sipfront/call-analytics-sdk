44c0053d1d71db6cdb22f7d3bb217a8f640a9ef3
Dominik Ridjic <dominik.ridjic@sparrowlabs.at>
2023-09-07 18:00:48 +0200

    Fixed crash on iOS
    * Added handling NullPointerException which wasn't listed in @Throws of method. iOS requires all possible Exception types to be listed, otherwise the Exception cannot be caught and leads to a crash

--- call-analytics/src/appleMain/kotlin/com/sipfront/sdk/SessionParams.apple.kt
+++ call-analytics/src/appleMain/kotlin/com/sipfront/sdk/SessionParams.apple.kt
@@ -8,11 +8,17 @@ import com.sipfront.sdk.log.Log
 import kotlinx.serialization.SerializationException
 import platform.Foundation.NSProcessInfo
 
-actual class SessionParams: ProguardKeep {
-    @Throws(SerializationException::class, IllegalArgumentException::class)
+actual class SessionParams : ProguardKeep {
+    @Throws(SerializationException::class, IllegalArgumentException::class, IllegalStateException::class)
     internal actual fun parse(): SessionConfig {
-        val json: String = NSProcessInfo.processInfo.environment[Keys.INITIALIZATION] as String
-        Log.debug()?.i("Received JSON SessionParams: $json")
-        return JsonParser.toObject<SessionConfig>(json)
+        try {
+            val json: String = NSProcessInfo.processInfo.environment[Keys.INITIALIZATION] as String
+            Log.debug()?.i("Received JSON SessionParams: $json")
+            return JsonParser.toObject<SessionConfig>(json)
+        } catch (e: SerializationException) {
+            throw e
+        } catch (e: Exception) {
+            throw IllegalStateException("Couldn't parse SessionParams: ${e.message}")
+        }
     }
 }
