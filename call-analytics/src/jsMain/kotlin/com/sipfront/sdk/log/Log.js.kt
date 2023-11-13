59cf7d3be4be4c0104100fcd91f03af9b25f8ffe
Dominik Ridjic <dominik.ridjic@sparrowlabs.at>
2023-11-13 12:23:34 +0100

    Fixed failure to log in Javascript

--- call-analytics/src/jsMain/kotlin/com/sipfront/sdk/log/Log.js.kt
+++ call-analytics/src/jsMain/kotlin/com/sipfront/sdk/log/Log.js.kt
@@ -1,7 +1,6 @@
 package com.sipfront.sdk.log
 
+import co.touchlab.kermit.ConsoleWriter
 import co.touchlab.kermit.LogWriter
-import co.touchlab.kermit.NoTagFormatter
-import co.touchlab.kermit.platformLogWriter
 
-internal actual fun getLogWriter(): LogWriter = platformLogWriter(NoTagFormatter)
+internal actual fun getLogWriter(): LogWriter = ConsoleWriter()
